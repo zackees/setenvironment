6a547bfb631dfb98a9508e5860bfcb6ac5e3e267
zackees <zachvorhies@protonmail.com>
2023-01-20 12:54:14 -0500

    adds macos tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,22 +4,32 @@ Test the main module
 
 # pylint: disable=fixme
 
+import sys
 import os
-import random
 import unittest
 
-from setenvironment import set_env_var
+HERE = os.path.dirname(__file__)
 
 
 class MainTester(unittest.TestCase):
     """Tester for the main module."""
 
-    def test_set_env(self) -> None:
+    @unittest.skipIf(sys.platform != "darwin", "Not macos")
+    def test_set_env_macos(self) -> None:
         """Test setting an environment variable."""
+        from setenvironment import setenv_macos  # pylint: disable=import-outside-toplevel
+
+        bashrc = os.path.join(HERE, "darwin.mybashrc")
+        # write a blank file
+        with open(bashrc, encoding="utf-8", mode="w") as file:
+            file.write("")
+
+        setenv_macos.get_target = lambda: bashrc  # type: ignore
+        setenv_macos.set_env_var("SETENVIRONMENT_TEST", "test")
+        setenv_macos.add_env_path("/my/path")
         # generate a random value
-        value = random.randint(0, 100)
-        set_env_var("SETENVIRONMENT_TEST", value)
-        self.assertEqual(value, int(os.environ["SETENVIRONMENT_TEST"]))
+
+        # self.assertEqual(value, int(os.environ["SETENVIRONMENT_TEST"]))
         # TODO add tests for getting the system path
 
 
