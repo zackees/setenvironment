3739e22b7820e5b7a46f461dcd4a19072a365724
zackees <z@zackees.com>
2023-10-06 16:57:40 -0700

    add twice add/remove

--- tests/test_template_path.py
+++ tests/test_template_path.py
@@ -88,6 +88,41 @@ class TemplatePathtester(unittest.TestCase):
             bashrc_str = read_utf8(bashrc)
             self.assertNotIn(mypath, bashrc_str)
 
+    def test_add_template_path_if_empty_add_twice(self) -> None:
+        """Test setting an environment variable."""
+        if sys.platform != "win32":
+            bashrc = os.path.join(HERE, "unix.mybashrc")
+            # write a blank file
+            with open(bashrc, encoding="utf-8", mode="w") as file:
+                file.write("")
+            set_env_config_file(bashrc)
+        key = "MYPATH"
+        mypath = os.path.join("my", "path")
+        mypath2 = os.path.join("my", "path2")
+        add_template_path(key, mypath)
+        add_template_path(key, mypath2)
+        new_paths = get_paths()
+        print("path is now:\n", new_paths)
+        if sys.platform != "win32":
+            system_key = f"${key}"
+        else:
+            system_key = f"%{key}%"
+        try:
+            self.assertIn(system_key, new_paths)
+            self.assertIn(mypath, get_env_var(key) or "")
+        except Exception as exc:
+            print(exc)
+            raise exc
+        finally:
+            remove_template_path(key, mypath, remove_if_empty=True)
+            remove_template_path(key, mypath2, remove_if_empty=True)
+        print(f"path after removals of {mypath} is now:\n{os.environ['PATH']}")
+        paths = get_paths()
+        self.assertNotIn(system_key, paths)
+        if sys.platform != "win32":
+            bashrc_str = read_utf8(bashrc)
+            self.assertNotIn(mypath, bashrc_str)
+
 
 if __name__ == "__main__":
     unittest.main()
