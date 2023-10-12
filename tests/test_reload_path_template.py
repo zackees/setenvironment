d14410b4f654973cfc561150cac13d9a629c7072
zackees <z@zackees.com>
2023-10-11 17:49:46 -0700

    adds more test

--- tests/test_reload_path_template.py
+++ tests/test_reload_path_template.py
@@ -14,14 +14,17 @@ from setenvironment.testing.basetest import BaseTest
 
 PATH_KEY = "TEST_RELOAD_PATH"
 MY_PATH = os.path.join("setenvironment", "test", "path")
+MY_PATH2 = os.path.join("setenvironment", "test", "path2")
 MY_VAR = ("SET_ENVIRONMENT_TEST_ENV_VAR", "foo")
 
 
 class ReloadPathTemplateTest(BaseTest):
     def tearDown(self) -> None:
         remove_template_path(PATH_KEY, MY_PATH)
+        remove_template_path(PATH_KEY, MY_PATH2)
+        os.environ.pop(PATH_KEY, None)
 
-    def test_paths(self) -> None:
+    def test_one_path(self) -> None:
         """Tests that we can add an environmental variable and then reload it."""
         remove_template_path(PATH_KEY, MY_PATH, remove_if_empty=True)
         self.assertNotIn(PATH_KEY, os.environ)
@@ -29,6 +32,27 @@ class ReloadPathTemplateTest(BaseTest):
         self.assertNotIn(PATH_KEY, os.environ)  # Should not be in os.environ yet.
         reload_environment()
         self.assertIn(PATH_KEY, os.environ)  # Should now be in os.environ.
+        self.assertIn(MY_PATH, os.environ[PATH_KEY])
+        if sys.platform == "win32":
+            system_key = f"%{PATH_KEY}%"
+        else:
+            system_key = f"${PATH_KEY}"
+        paths = os.environ["PATH"].split(os.pathsep)
+        self.assertIn(system_key, paths)
+
+
+    def test_two_path(self) -> None:
+        """Tests that we can add an environmental variable and then reload it."""
+        remove_template_path(PATH_KEY, MY_PATH, remove_if_empty=True)
+        remove_template_path(PATH_KEY, MY_PATH2, remove_if_empty=True)
+        self.assertNotIn(PATH_KEY, os.environ)
+        add_template_path(PATH_KEY, MY_PATH, update_curr_environment=False)
+        add_template_path(PATH_KEY, MY_PATH2, update_curr_environment=False)
+        self.assertNotIn(PATH_KEY, os.environ)  # Should not be in os.environ yet.
+        reload_environment()
+        self.assertIn(PATH_KEY, os.environ)  # Should now be in os.environ.
+        self.assertIn(MY_PATH, os.environ[PATH_KEY])
+        self.assertIn(MY_PATH2, os.environ[PATH_KEY])
         if sys.platform == "win32":
             system_key = f"%{PATH_KEY}%"
         else:
