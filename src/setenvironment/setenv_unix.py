33e541019c8c56ffb91214ce41e11327f3fa28ca
Zachary Vorhies <zachvorhies@protonmail.com>
2023-10-12 13:14:57 -0700

    fix unix

--- src/setenvironment/setenv_unix.py
+++ src/setenvironment/setenv_unix.py
@@ -147,9 +147,7 @@ def unset_env_var(name: str) -> None:
         set_bash_file_lines(lines, settings_file)
 
 
-def add_env_path(
-    path: str, verbose: bool = False, update_curr_environment: bool = True
-) -> None:
+def add_env_path(path: str, verbose: bool = False, update_curr_environment: bool = True) -> None:
     """Adds a path to the PATH environment variable."""
     path_list = os.environ["PATH"].split(os.path.sep)
     if path not in path_list and update_curr_environment:
@@ -190,9 +188,7 @@ def remove_env_path(path: str, update_curr_environment=True) -> None:
         set_bash_file_lines(lines, settings_file)
 
 
-def add_template_path(
-    env_var: str, new_path: str, update_curr_environment=True
-) -> None:
+def add_template_path(env_var: str, new_path: str, update_curr_environment=True) -> None:
     assert "$" not in env_var, "env_var should not contain $"
     assert "$" not in new_path, "new_path should not contain $"
     path_str = get_env_var("PATH")
@@ -224,9 +220,7 @@ def add_template_path(
         )
 
 
-def remove_template_path(
-    env_var: str, path_to_remove: str, remove_if_empty: bool
-) -> None:
+def remove_template_path(env_var: str, path_to_remove: str, remove_if_empty: bool) -> None:
     assert "$" not in env_var, "env_var should not contain $"
     assert "$" not in path_to_remove, "path_to_remove should not contain $"
     var_paths = parse_paths(get_env_var(env_var) or "")
@@ -254,6 +248,7 @@ def reload_environment(verbose: bool) -> None:
     for key, val in env_vars.items():
         if key == "PATH":
             continue
+        os.environ[key] = val
     path_list = [os.path.expandvars(path) for path in path_list]
     path_list_str = os.path.pathsep.join(path_list)
     os.environ["PATH"] = path_list_str
