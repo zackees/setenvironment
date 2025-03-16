e1972858344e0acd8d59457ad5443461ae5b8b84
zackees <z@zackees.com>
2025-03-15 19:11:00 -0700

    patch upload.sh

--- upload_package.sh
+++ upload_package.sh
@@ -1,7 +1,7 @@
-#twine upload -r testpypi dist/* --verbose
+#!/bin/bash
+set -e
 rm -rf build dist
-echo "Building Source and Wheel (universal) distribution…"
-python setup.py sdist bdist_wheel --universal
-echo "Uploading the package to PyPI via Twine…"
-twine upload dist/* --verbose
+uv pip install wheel twine
+uv build --wheel
+uv run twine upload dist/* --verbose
 # echo Pushing git tags…
